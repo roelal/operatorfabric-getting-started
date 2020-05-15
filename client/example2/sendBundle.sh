63cab1cd08ade7110b50f4a9dbaab9e5bdcca884
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2020-05-15 15:13:41 +0200

    [OC-896] Update script and minor fixes

--- client/example2/sendBundle.sh
+++ client/example2/sendBundle.sh
@@ -1,2 +1,1 @@
-token=INSERT YOUR TOKEN
 curl -s -X POST "http://localhost:2100/thirds" -H  "accept: application/json" -H  "Content-Type: multipart/form-data" -H "Authorization:Bearer $token" -F "file=@bundle.tar.gz;type=application/gzip"
